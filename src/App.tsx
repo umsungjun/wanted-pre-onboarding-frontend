9749f0b135f6fbfec9f51fd30c5534298259409e
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-06-10 02:04:32 +0900

    로그인 path 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from "./Pages/SignUp";
+import SignIn from "./Pages/SignIn";
 function App() {
-  return <div className="App">초기</div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
