986de47ca8b3fd34e8a929a23920da756eee8809
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-06-10 02:54:23 +0900

    todo path 지정

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUp from "./Pages/SignUp";
 import SignIn from "./Pages/SignIn";
+import ToDO from "./Pages/ToDO";
 function App() {
   return (
     <BrowserRouter>
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
+          <Route path="/todo" element={<ToDO />} />
         </Routes>
       </div>
     </BrowserRouter>
