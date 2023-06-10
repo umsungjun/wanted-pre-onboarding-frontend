3929bcdc08cb03b9a93fdf70e1bc487a39460949
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-06-10 22:57:08 +0900

    로그인 디자인 완료

--- src/Pages/SignIn.tsx
+++ src/Pages/SignIn.tsx
@@ -38,27 +38,35 @@ export default function SignIn() {
     }
   };
   return (
-    <>
-      이메일 :
-      <input
-        data-testid="email-input"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <br />
-      비밀번호 :
-      <input
-        data-testid="password-input"
-        type="password"
-        onChange={(e) => setPassWord(e.target.value)}
-      />
-      <br />
+    <section className="flex gap-5 flex-col content-center justify-center rounded-2xl w-96 bg-slate-50 px-7 py-5">
+      <h1 className="text-center text-3xl">로그인</h1>
+      <div className="flex items-center">
+        <span className="text-center w-20 mr-2 ">이메일 :</span>
+        <input
+          data-testid="email-input"
+          placeholder="Email"
+          className="flex-1 py-2 pl-3  rounded-md border border-gray-300 focus:border-blue-700 focus:outline-none focus:ring-1 focus:ring-blue-700 py-1 px-1.5 text-gray-500"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="flex items-center">
+        <span className="text-center w-20 mr-2">비밀번호 :</span>
+        <input
+          data-testid="password-input"
+          type="password"
+          placeholder="PassWord"
+          className="flex-1 pl-3  py-2 rounded-md border border-gray-300 focus:border-blue-700 focus:outline-none focus:ring-1 focus:ring-blue-700 py-1 px-1.5 text-gray-500"
+          onChange={(e) => setPassWord(e.target.value)}
+        />
+      </div>
       <button
         data-testid="signin-button"
         disabled={btnDisable}
         onClick={(e) => handleLogin(e)}
+        className="bg-blue-600 py-3 rounded-lg text-white mt-2"
       >
         로그인
       </button>
-    </>
+    </section>
   );
 }
