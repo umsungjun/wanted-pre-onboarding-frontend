e0cd93eb4f5d444236d2cef64019f4e6cef643f1
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-06-10 22:53:09 +0900

    회원가입 디자인 완료

--- src/Pages/SignUp.tsx
+++ src/Pages/SignUp.tsx
@@ -38,22 +38,24 @@ export default function SignUp() {
   };
 
   return (
-    <section className="flex gap-2 flex-col content-center justify-center rounded-l-md bg-white">
-      <div className="flex">
-        <span>이메일 :</span>
+    <section className="flex gap-5 flex-col content-center justify-center rounded-2xl w-96 bg-slate-50 px-7 py-5">
+      <h1 className="text-center text-3xl">회원가입</h1>
+      <div className="flex items-center">
+        <span className="text-center w-20 mr-2 ">이메일 :</span>
         <input
           data-testid="email-input"
-          placeholder="Enter your email"
-          className="block rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500"
+          placeholder="Email"
+          className="flex-1 py-2 pl-3  rounded-md border border-gray-300 focus:border-blue-700 focus:outline-none focus:ring-1 focus:ring-blue-700 py-1 px-1.5 text-gray-500"
           onChange={(e) => setEmail(e.target.value)}
         />
       </div>
-      <div className="flex">
-        <span>비밀번호 :</span>
+      <div className="flex items-center">
+        <span className="text-center w-20 mr-2">비밀번호 :</span>
         <input
           data-testid="password-input"
           type="password"
-          className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500"
+          placeholder="PassWord"
+          className="flex-1 pl-3  py-2 rounded-md border border-gray-300 focus:border-blue-700 focus:outline-none focus:ring-1 focus:ring-blue-700 py-1 px-1.5 text-gray-500"
           onChange={(e) => setPassWord(e.target.value)}
         />
       </div>
@@ -61,6 +63,7 @@ export default function SignUp() {
         data-testid="signup-button"
         disabled={btnDisable}
         onClick={(e) => handleJoin(e)}
+        className="bg-blue-600 py-3 rounded-lg text-white mt-2"
       >
         회원가입
       </button>
